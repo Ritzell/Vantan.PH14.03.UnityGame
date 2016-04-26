6e3f77d2e4217d5f3375fd723f9d10d1a5743187
河島有志 <kawasimayuji@gmail.com>
2016-04-26 14:12:42 +0900

    昼空実装。直線ミサイル(物理)実装。機体の動作実装。 次回は直線ミサイルを数値移動させるのと、プレハブにして無限発射させる。

---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour {
+	private float speed = 80.0f;
+	public	GameObject	body;
+	public	GameObject 	myCamera;
+	public	GameObject	look;
+	engineSound engineS;
+	// Use this for initialization
+	void Start () {
+		engineS = GameObject.Find("engine").GetComponent<engineSound> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float mY =  Input.GetAxis ("Horizontal");
+		float mX = Input.GetAxis ("Vertical");
+
+		//myCamera.transform.LookAt(look.transform);
+		transform.Translate (Vector3.forward * Time.deltaTime * speed);
+		transform.Rotate (mX/2, 0f, mY*3);
+
+		if (Input.GetKey(KeyCode.Joystick1Button4)) {
+			Speed= -1f;
+		}
+
+		if (Input.GetKey(KeyCode.Joystick1Button5)) {
+			Speed=+1f;
+		}
+	}
+	public float Speed {
+		set{
+			if (this.speed >= 60f && this.speed <= 280f) {
+				this.speed += value;
+			}
+			if (this.speed < 60) {
+				this.speed = 60f;
+			} else if (this.speed > 280) {
+				this.speed = 280f;
+			}
+			engineS.Pitch = this.speed;
+		}get{
+			if (this.speed < 60) {
+				return 60f;
+			}
+			return this.speed;
+		}
+	}
+}
---
+++ Assets/engineSound.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class engineSound : MonoBehaviour {
+	AudioSource audio;
+	private float pitch = 1;
+	// Use this for initialization
+	void Start () {
+		audio = GetComponent<AudioSource> ();
+		audio.pitch = pitch;
+	}
+
+	public float Pitch{
+		set{
+			float v = (value - 80f)*0.01f;
+			if (pitch <= 3 && pitch >= 0.75f) {
+				pitch = v + 1;
+				if (pitch > 3) {
+					pitch = 3;
+				} else if (pitch < 0.75f) {
+					pitch = 0.75f;
+				}
+			}
+			audio.pitch = this.pitch;
+		}get{
+			return pitch;
+		}
+	}
+}
