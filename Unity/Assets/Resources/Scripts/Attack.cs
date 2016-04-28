995950a9fc6ed62e9fd673f8fecac6ca10e19277
河島有志 <kawasimayuji@gmail.com>
2016-04-28 12:34:56 +0900

    .gitignoreを追加

---
+++ Unity/Assets/Resources/Scripts/Attack.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Attack : MonoBehaviour {
+	public static Queue<GameObject> missiles = new Queue<GameObject> ();
+	//public GameObject[] missiles = new GameObject[4];
+	
+	// Use this for initialization
+	void Start () {
+		missiles.Enqueue (GameObject.Find ("missileA"));
+		missiles.Enqueue (GameObject.Find ("missileB"));
+		missiles.Enqueue (GameObject.Find ("missileC"));
+		missiles.Enqueue (GameObject.Find ("missileD"));
+        StartCoroutine(shoot());
+	}
+
+    public IEnumerator shoot()
+    {
+       float reloading = 0.5f;
+        while (!GameManager.GameOver)
+        {
+            reloading += Time.deltaTime;
+            if(reloading >= 0.3f)
+            {
+                if ((Input.GetAxis("RTrigger") == 1 || Input.GetKeyDown(KeyCode.C)) && missiles.Count >= 1)
+                {
+                    StartCoroutine(missiles.Dequeue().GetComponent<missile>().straight());
+                    reloading = 0f;
+                }
+            }
+            yield return null;
+        }
+        Debug.Log("end");
+        yield return null;
+    }
+}
---
+++ Unity/Assets/Resources/Scripts/missile.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class missile : MonoBehaviour {
+	public AudioClip audioClip1;
+	public AudioClip audioClip2;
+	private float speed = 500f;
+	private Vector3 startPos;
+	private Quaternion startRot;
+	private static Transform root;
+	private AudioSource audioS;
+
+	void Start () {
+		audioS = gameObject.GetComponent<AudioSource>();
+		audioS.clip = audioClip1;
+		startPos = transform.localPosition;
+		startRot = transform.localRotation;
+		root = GameObject.Find ("missiles").transform;
+	}
+	
+	// Update is called once per frame
+
+	public IEnumerator straight(){
+        StartCoroutine (reloadMissile());
+		transform.parent = null;
+        audioS.Play();
+        for (float timer = 0f; timer < 0.1f;timer += Time.deltaTime)
+        {
+            transform.Translate(0, -0.1f, 0);
+            yield return null;
+        }
+        while (true){
+            try
+            {
+                transform.Translate(Vector3.back * Time.deltaTime * speed);
+            }catch{}
+			yield return null;
+		}
+	}
+
+	void OnTriggerEnter(Collider col){
+		Debug.Log ("missileHit!");
+	}
+
+	private IEnumerator reloadMissile(){
+		float timer = 0f;
+		while (timer < 3f) {
+			timer += Time.deltaTime;
+			yield return null;
+		}
+		GameObject newMissile = (GameObject)Instantiate (Resources.Load("prefabs/"+gameObject.name),Vector3.zero,Quaternion.identity);
+        newMissile.transform.parent = root;
+		newMissile.name = name.Substring (0,8);
+		newMissile.transform.localPosition = startPos;
+		newMissile.transform.localRotation = startRot;
+        yield return new WaitForSeconds(0.35f);
+        Attack.missiles.Enqueue(newMissile);
+        yield return new WaitForSeconds(10);
+		Destroy (gameObject);
+		yield return null;
+	}
+}
