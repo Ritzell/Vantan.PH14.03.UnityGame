a701c4292bd04d02c66f45eba53e441a7aed6256
河島有志 <kawasimayuji@gmail.com>
2016-04-29 23:40:18 +0900

    レティクルに1.5秒以上入れるとロックン対象になるプログラムを追加。

--- Unity/Assets/Resources/Scripts/ReticleSystem.cs
+++ Unity/Assets/Resources/Scripts/ReticleSystem.cs
@@ -10,23 +10,46 @@ public class ReticleSystem : MonoBehaviour {
 	//int layerMask = 1 << 8 | 1 << 9;
 	////Corgi(8)レイヤーとだけ衝突しない
 	//int layerMask = ~(1 << 8);
+	public static GameObject lockOnTgt;
+	private float lockNow = 0f;
 	public Camera MainCamera, UICamera;
 	RectTransform rt = null;
 	public GameObject reticleUI;
 	// Use this for initialization
 	void Start () {
 		rt = GetComponent<RectTransform> ();
-		StartCoroutine (lockOn ());
+		StartCoroutine (serchEnemy ());
 	}
-	public IEnumerator lockOn(){
+	public IEnumerator serchEnemy(){
 		RaycastHit hit;
 		int layerMask = 1 << 11 | 1 << 12;
 		while (!GameManager.GameOver) {
 			var ray = RectTransformUtility.ScreenPointToRay (Camera.main,new Vector3(294.0f, 157.5f, 0.0f));
-			if(Physics.Raycast(ray,out hit,30000,layerMask)){
-				Debug.Log (hit.transform.name);
+			if(Physics.Raycast(ray,out hit,30000,layerMask) && lockOnTgt == null){
+				LockNow = hit.transform.gameObject;//Time.deltaTime;
 			}
 			yield return null;
+		}
+	}
+
+	public GameObject LockNow{
+		set{
+			if (Time.deltaTime >= 0.4f) {
+				Debug.Log (lockNow);
+				lockNow = 0f;
+			} else {
+				lockNow += Time.deltaTime;
+				LockOn (value);
+			}
+		}get{
+			return null;
+		}
+	}
+	public void LockOn(GameObject tgt){
+		if (lockNow >= 1.5f) {
+			lockOnTgt = tgt;
+			lockNow = 0;
+			Debug.Log (lockOnTgt);
 		}
 	}
 }
