cb6db1b6ae61d3f4833bb49a9177e42c228e5c0f
Yuji Kawashima <kawasimayuji@gmail.com>
2016-08-21 20:22:36 +0900

    タイトル画面にVRモードとノーマルモードの切り替えボタンを実装。 VRを仮実装。コントローラー未対応。

---
+++ Unity/Assets/VRMode.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class VRMode : MonoBehaviour
+{
+    private static Toggle VRToggle;
+    [SerializeField]
+    private GameObject VRCameraOb, NormalCameraOb;
+    [SerializeField]
+    private Canvas canvas;
+    [SerializeField]
+    private Camera VRCamera, NormalCamera;
+    void Awake()
+    {
+        VRCameraOb.SetActive(false);
+        VRToggle = gameObject.GetComponent<Toggle>();
+
+    }
+    public void isVR()
+    {
+        ChangePlayMode(VRToggle.isOn);
+    }
+
+    public void ChangePlayMode(bool isVR)
+    {
+        VRCameraOb.SetActive(isVR);
+        NormalCameraOb.SetActive(!isVR);
+        canvas.worldCamera = isVR ? VRCamera : NormalCamera;
+    }
+}
