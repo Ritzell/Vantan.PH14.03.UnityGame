9ebbf168fdda8fb3cc9257d8ebf59a3773414cea
河島有志 <kawasimayuji@gmail.com>
2016-09-08 14:09:18 +0900

    設定画面からマスターボリュームを調整できるようにした。

---
+++ Unity/Assets/AudioSetting.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioSetting : MonoBehaviour {
+	
+	public void OnValueChanged(float value){
+		Debug.Log (value);
+		AudioListener.volume = value;
+	}
+}
--- Unity/Assets/Scripts/MissileSystem.cs
+++ Unity/Assets/Scripts/MissileSystem.cs
@@ -63,7 +63,7 @@ public class MissileSystem : MonoBehaviour
 		if (gameObject.layer == 12) {
 			AudioS.volume = 0.5f;
 			AudioS.spatialBlend = 1;
-			AudioS.maxDistance = 250f;
+			AudioS.maxDistance = 200f;
 			GameManager.EnemyMIssilesCount += 1;
 			AudioS.clip = AudioClip3;
 			AudioS.loop = true;
